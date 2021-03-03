9b847bf12f8b57e1a975848fc8a44ee07acd4e29
AsherPop <77747352+AsherPop@users.noreply.github.com>
2021-03-03 11:03:38 -0800

    Add files via upload

---
+++ PoppletonPP3.py
@@ -0,0 +1,44 @@
+import random
+
+dogs = []
+dogGender = []
+dogSize = []
+dogColor = []
+
+
+while True:
+    dogNumber = input("How many dogs do you want?     ")
+    dogSize = list(random.randint(range, ["Mini", "Small", "Medium", "Large", "Gargantuan"])
+    dogGender = list(random.randint(range, ["Male", "Female"]))
+    dogColor = list(random.randint(range, ["Brown", "Black", "White", "Blonde", "Grey", "Black and White", "Black and Tan"])
+
+
+
+    for x in range(0, int(dogNumber)):
+                    dogs.append(random.randint(0, int(dogNumber)))
+
+
+    for x in range(0, str(dogSize)):
+                    dogs.append(random.randint(0, str(dogSize)))
+
+
+    for x in range(0, (dogColor))
+                    dogs.append(random.randint(0, str(dogColor)))
+
+
+    for x in range(0, (dogGender))
+                    dogs.append(random.randint(0, str(dogGender)))
+
+    if x > 8:
+        print("To be honest with you, if you have that many dogs you are a psycho.")
+
+
+
+
+
+
+
+    print("Here are the dogs you are going to buy: {}".format(dogs))
+                                
+                        
+                    
