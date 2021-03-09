5d06c28ae6886d96daaff86ceeac0856b121055e
AsherPop <77747352+AsherPop@users.noreply.github.com>
2021-03-09 12:03:29 -0700

    Add files via upload

---
+++ PoppletonListAppV1.py
@@ -0,0 +1,30 @@
+"""
+program goals:
+
+-get input from user
+-conver input into INT
+-add that input into a list
+-pull values from specific index positions\
+
+"""
+
+#create functions that perform those actions above
+
+def mainProgram():
+    myList = []
+    print("Hello there my friend! Lets get working with lists!")
+    print("Choose one of the following options. type a number ONLY please!")
+    choice = input("1. Add to list, 2. Return the value at an index position        ")
+    if choice == "1":
+        addToList()
+
+    elif choice == "2":
+        indexValues
+    #need to add way to exit the program and catch errors made by the user
+
+def addToList():
+
+def indexValues():
+
+if __name__ == "__main__":
+    mainProgram()
