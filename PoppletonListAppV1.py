42889df2b3495b18dff13feadc8df7e69861adf9
AsherPop <77747352+AsherPop@users.noreply.github.com>
2021-03-18 11:04:12 -0700

    Add files via upload

--- PoppletonListAppV1.py
+++ PoppletonListAppV1.py
@@ -18,23 +18,34 @@ def mainProgram():
             print("Hello there my friend! Lets get working with lists!")
             print("Choose one of the following options. Type a number ONLY please!")                                                            
             choice = input("""1. Add to list
-    2. Return the value at an index position
-    3. Random Choice
-    4. End Program       """)
+2. Add a bunch of numbers
+3. Return the value at an index position
+4. Print contents of list
+5. Random Choice
+6. Linear Search
+7. End Program       """)
             if choice == "1":
                 addToList()
 
             elif choice == "2":
-                indexValues()
+                addABunch()
 
             elif choice == "3":
-                randomSearch()
+                indexValues()
 
             elif choice == "4":
-                break
+                print(myList)
+
+            elif choice == "5":
+                randomSearch()
+
+            elif choice == "6":
+                linearSearch()
+               
 
             else:
-                print("You did not select an option available, please try again(Just kidding I'm not a robot:) have fun!)")
+                break
+        
 
         except:
             print("An error ocurred when you tried to complete this action")
@@ -48,11 +59,30 @@ def addToList():
     myList.append(int(newItem))
     print(myList)
 
-    
+
+
+def addABunch():
+    print("We're gonna add a bunch of numbers!!!")
+    numToAdd = input("How many integers do you want to add?       ")
+    numRange = input("How high would you like these numbers to go?      ")
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is now complete!")
+        
 
 def indexValues():
     indexPos = input("Which index position would you like to view within your list     ")
     print(myList[int(indexPos)])
+
+
+
+def linearSearch():
+    print("We're going to search through the list in the worst way possible!")
+    searchItem = input("What are you looking for? Number-wise?    ")
+    for x in range(len(myList)):
+        if myList[x] == int(searchItem):
+            print("Your item is at index {}".format(x))
+
 
 
 
